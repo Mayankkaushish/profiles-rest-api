6528100abade300dc849ccc82b56448bbc4f64c7
Mayank kaushish <mayank.kaushish@gmail.com>
2020-07-21 22:08:58 +1000

    Test API view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+	"""Test API View"""
+	def get(self,request,format=None):
+		"""Returns API list view features"""
+		an_apiview=['Red','Yelow','Orange','White']
+
+		return Response({'message':'Hello','an_apiview': an_apiview})
